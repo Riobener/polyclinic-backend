8de7a689b28df6c0a68e90c9de7e765f8f5d52b7
elkin <elk.iv@ya.ru>
2023-01-16 23:09:32 +0300

    Payment fix

--- payment-service/src/main/kotlin/com/polyclinic/paymentservice/application/PaymentController.kt
+++ payment-service/src/main/kotlin/com/polyclinic/paymentservice/application/PaymentController.kt
@@ -1,10 +1,11 @@
+package com.polyclinic.paymentservice.application
+
 import com.polyclinic.paymentservice.domain.entities.JpaPayment
 import com.polyclinic.paymentservice.infrastructure.services.PaymentService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
-
 
 @RestController
 
@@ -13,25 +14,29 @@ class PaymentController(
     @Autowired
     private val paymentService: PaymentService,
 ) {
-    @PostMapping("/payments/markPaid", "application/json")
+    @GetMapping("/health")
+    fun checkHealth() = "All good"
+
+    @PostMapping("/payments/markPaid")
     fun markAsPaid(@RequestParam applicationId: UUID): ResponseEntity<*>? {
         val result = paymentService.markAsPaid(applicationId)
         return ResponseEntity.ok(result)
     }
 
-    @PostMapping("/payments/save", "application/json")
+    @PostMapping("/payments/save")
     fun save(@RequestBody payment: JpaPayment): ResponseEntity<*>? {
-        val result = paymentService.save(payment)
-        return ResponseEntity.ok(result)
+        paymentService.save(payment)
+        return ResponseEntity.ok()
+            .body("Платёж успешно сохранён")
     }
 
-    @GetMapping("/payments/byApplication", "application/json")
+    @GetMapping("/payments/byApplication")
     fun findByApplicationId(@RequestParam applicationId: UUID): ResponseEntity<*>? {
         val result = paymentService.findByApplicationId(applicationId)
         return ResponseEntity.ok(result)
     }
 
-    @GetMapping("/payments/byId", "application/json")
+    @GetMapping("/payments/byId")
     fun findById(@RequestParam applicationId: UUID): ResponseEntity<*>? {
         val result = paymentService.findById(applicationId)
         return ResponseEntity.ok(result)
